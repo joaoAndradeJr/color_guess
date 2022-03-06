77abf3fe9633081508f96634d8e2b7c6a39136c7
joaoAndradeJr <fullservicemanaus@gmail.com>
2022-03-05 20:06:37 -0400

    feat: req 4 - Adicione cores nas bolas elas devem ser geradas dinâmicamente

--- script.js
+++ script.js
@@ -5,3 +5,28 @@ for (let index = 0; index < 6; index += 1) {
   div.className = 'ball';
   ballsParent.appendChild(div);
 }
+
+function randomNumber() {
+  const min = Math.ceil(0);
+  const max = Math.floor(255);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function generateColor() {
+  const r = randomNumber();
+  const g = randomNumber();
+  const b = randomNumber();
+  return `rgb(${r}, ${g}, ${b})`;
+}
+
+function setBackground() {
+  const balls = document.querySelectorAll('.ball');
+
+  for (let index = 0; index < 6; index += 1) {
+    balls[index].style.backgroundColor = generateColor();
+  }
+}
+
+window.onload = () => {
+  setBackground();
+};
