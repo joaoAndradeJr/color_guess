fef0eea2df35392c0dcea39122cdef7c707c3494
joaoAndradeJr <fullservicemanaus@gmail.com>
2022-03-05 20:41:58 -0400

    feat: req 5 - Clicar em um circulo colorido, deve ser mostrado um texto indicando se está correto

--- script.js
+++ script.js
@@ -1,32 +1,58 @@
 const ballsParent = document.querySelector('#balls');
 
+function showAnswer(text) {
+  document.querySelector('#answer').innerText = text;
+}
+
+function checkAnswer(target) {
+  const color = document.querySelector('#rgb-color').innerText;
+  const guess = target.style.backgroundColor;
+
+  if (color === guess) {
+    showAnswer('Acertou!');
+  } else {
+    showAnswer('Errou! Tente novamente!');
+  }
+}
+
 for (let index = 0; index < 6; index += 1) {
   const div = document.createElement('div');
   div.className = 'ball';
+  div.addEventListener('click', ({ target }) => checkAnswer(target));
   ballsParent.appendChild(div);
 }
 
-function randomNumber() {
-  const min = Math.ceil(0);
-  const max = Math.floor(255);
+function randomNumber(a, b) {
+  const min = Math.ceil(a);
+  const max = Math.floor(b);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 function generateColor() {
-  const r = randomNumber();
-  const g = randomNumber();
-  const b = randomNumber();
+  const r = randomNumber(0, 255);
+  const g = randomNumber(0, 255);
+  const b = randomNumber(0, 255);
   return `rgb(${r}, ${g}, ${b})`;
+}
+
+const colors = [];
+for (let index = 0; index < 6; index += 1) {
+  colors.push(generateColor());
+}
+
+function colorToGuess() {
+  const position = randomNumber(0, 5);
+  document.getElementById('rgb-color').innerText = colors[position];
 }
 
 function setBackground() {
   const balls = document.querySelectorAll('.ball');
-
   for (let index = 0; index < 6; index += 1) {
-    balls[index].style.backgroundColor = generateColor();
+    balls[index].style.backgroundColor = colors[index];
   }
 }
 
 window.onload = () => {
+  colorToGuess();
   setBackground();
 };
